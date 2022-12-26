c43d6f295e569f0d1c6d85124be77b0a463b2dd4
KajiyamaVK <victor.kajiyama@gmail.com>
2022-12-26 15:48:43 -0300

    Managing bug

--- pages/componentes_ui/index.jsx
+++ pages/componentes_ui/index.jsx
@@ -1,9 +1,6 @@
 import style from './stylesheet.module.css'
 import dynamic from 'next/dynamic'
 import { Button } from '@mui/material'
-var Editor = dynamic(() => import('../../public/components/Editor'), {
-    ssr: false
-  })
 
 export default function AdminPage(){
 
@@ -11,7 +8,7 @@ export default function AdminPage(){
     
     return(
         <>
-        <Editor />
+
         
         </>
     )
