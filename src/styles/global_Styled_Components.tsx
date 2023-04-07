493c873406775d563505ed1c59fad61e1089ccb7
victor.kajiyama <victor.kajiyama@sinqia.com.br>
2023-04-07 11:09:28 -0300

    Alterando o tamanho padrão do REM da fonte

---
+++ src/styles/global_Styled_Components.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center; 
+    max-width: 1000px;
+    margin: 0 auto;
+    height: 70vh;
+    position: relative;
+    padding: 0 100px;
+`;
+
+export { Main };
