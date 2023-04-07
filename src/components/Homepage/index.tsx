7c11b3a597aaab8e6fa9b93055a3225cb7fec1a3
victor.kajiyama <victor.kajiyama@sinqia.com.br>
2023-04-07 11:13:16 -0300

    Switching the word 'grettings' to HELLO since it was to big for mobile

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -6,7 +6,7 @@ const rubik = Rubik({ subsets: ['latin'] })
 export default function MainPage() {
     return (
         <>
-            <Greeting>Saudações! Meu nome é</Greeting>
+            <Greeting>Olá! Meu nome é</Greeting>
             <Name>Victor Kajiyama.</Name>
             <BigDescription>Sou um arquiteto da web</BigDescription>
             <SmallDescription>Desenvolvedor de sites e sistemas para a web com foco principal em criar interfaces que geram uma experiência prática e moderna ao usuário - sempre
