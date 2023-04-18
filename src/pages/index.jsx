1e4e8261fbbed285ea422c519ab67b59aa803416
victor.kajiyama <victor.kajiyama@sinqia.com.br>
2023-04-18 15:39:15 -0300

    Adding meta tags to create thumbnails

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -43,6 +43,30 @@ export default function Home() {
     <div className={rubik.className}>
       <Head>
         <title>Victor Kajiyama</title>
+        {/* Microsoft Meta Tags */}
+        <meta name="msapplication-TileColor" content="#ffffff" />
+
+        {/* Facebook and Whatsapp Metatags */}
+        {/* DIsplay site name, title and description */}
+        <meta property="og:site_name" content="Victor Kajiyama - Fullstacker" />
+        <meta property="og:title" content="Seu website por BAIXO CUSTO" />
+        <meta
+          property="og:description"
+          content="Sua ideia + meu portfolio = preço baixo"
+        />
+
+        {/* Image to display in the thumbnails */}
+        <meta property="og:image" itemProp="image" content="/imgs/logo.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/png" />
+
+        {/* Any size bigger than 300px will not work on Whatsapp */}
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+
+        {/* URL to visit when clicking on the thumbnail */}
+        <meta property="og:url" content="https://fullstacker.com.br" />
+
         <meta
           name="description"
           content="Site criado para portfolio de Victor Koiti Kajiyama"
